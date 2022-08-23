3c447c72b02f6af4ba53eede64c689c4128753b1
DariaCrypto <dariada.crypton@gmail.com>
2022-08-23 17:47:08 +0800

    Refactoring test

--- tests/staking_test.py
+++ tests/staking_test.py
@@ -2,17 +2,36 @@ import pytest
 
 from brownie import AToken, BToken, RewardToken, Staking, StakeToken, accounts
 
+CLAIM_TIME = 600
+UNSTAKE_TIME = 1200
 
-def test_preprocess():
-    claimTime = 600
-    unstakeTime = 1200
-    stakeToken = StakeToken.deploy({'from': accounts[0]})
-    aToken = AToken.deploy(accounts[0], 100e18, {'from': accounts[0]})
-    bToken = BToken.deploy(accounts[0], 100e18, {'from': accounts[0]})
-    rewardToken = RewardToken.deploy(
-        accounts[0], 100e18, {'from': accounts[0]})
-    staking = Staking.deploy(bToken.address, rewardToken.address,
-                             claimTime, unstakeTime, 30, {'from': accounts[0]})
 
+@pytest.fixture
+def stakeToken():
+    return accounts[0].deploy(StakeToken)
+
+
+@pytest.fixture
+def aToken():
+    return accounts[0].deploy(AToken, accounts[0], 100e18)
+
+
+@pytest.fixture
+def bToken():
+    return accounts[0].deploy(BToken, accounts[0], 100e18)
+
+
+@pytest.fixture
+def rewardToken():
+    return accounts[0].deploy(RewardToken, accounts[0], 100e18)
+
+
+@pytest.fixture
+def staking(bToken, rewardToken):
+    return accounts[0].deploy(Staking, bToken.address, rewardToken.address,
+                              CLAIM_TIME, UNSTAKE_TIME, 30)
+
+
+def test_staking_info(staking):
     infoStake = staking.infoStake.call({'from': accounts[0]})
-    assert infoStake == (claimTime, unstakeTime, 30)
+    assert infoStake == (CLAIM_TIME, UNSTAKE_TIME, 30)
